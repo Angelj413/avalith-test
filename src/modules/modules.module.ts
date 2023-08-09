3d48a19eb3d887dc3cab2aeccba71468b5c2985f
Angel Morante <morante413@gmail.com>
2023-08-09 17:01:23 -0400

    feat :admins init

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, AdminsModule],
   providers: [],
   exports: [AuthModule],
 })
